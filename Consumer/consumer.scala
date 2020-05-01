5effe0bc4d290d8d76ee5d2fa211f3555c07363c
Rayane AMROUCHE <rayane.amrouche@epita.fr>
2020-05-01 16:58:57 +0200

    [CONS] added kafka consumer

--- Consumer/consumer.scala
+++ Consumer/consumer.scala
@@ -6,6 +6,52 @@ import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 
 object Consumer extends App {
+  
+  val TOPIC = "DroneStream"
 
+  val sparkConf = new SparkConf()
+    .setAppName("Spark")
+    .setMaster("local[*]")
 
+  val sparkSession = SparkSession
+    .builder()
+    .config(sparkConf)
+    .getOrCreate()
+
+  val kafkaProps = new Properties()
+  kafkaProps.put("bootstrap.servers", "localhost:9213")
+  kafkaProps.put(
+    "key.deserializer",
+    "org.apache.kafka.common.serialization.StringDeserializer"
+  )
+  kafkaProps.put(
+    "value.deserializer",
+    "org.apache.kafka.common.serialization.StringDeserializer"
+  )
+  kafkaProps.put("group.id", "drone")
+
+  val consumer = new KafkaConsumer[String, String](kafkaProps)
+
+  consumer.subscribe(util.Collections.singletonList(TOPIC))
+
+  
+  while(true){
+    val records = consumer.poll(500)
+    //println(records)
+    records.asScala.foreach{r =>
+	  //val df = jsonStrToMap(r.value())
+	  println(r)
+      //if(df("Alert") == 1){
+        //val record = new ProducerRecord[String, String]("alert_info", "key", r.value())
+        //val record1 = new ProducerRecord[String, String]("storage_info", "key", r.value())
+        //producer.send(record)
+        //producer.send(record1)
+        //println(df("Alert"))
+      // }else{
+      //   val record = new ProducerRecord[String, String]("storage_info", "key", r.value())
+      //   //producer.send(record)
+      //   println("Pas d'alerte")
+      // }
+    }
+  }
 }
