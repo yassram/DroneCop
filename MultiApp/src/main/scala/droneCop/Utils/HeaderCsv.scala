2a4e253a3778d90009c995b4991627b714e8cc1f
Rayane AMROUCHE <rayane.amrouche@epita.fr>
2020-05-10 03:47:46 +0200

    [UTILS] utils update with header for rdd

---
+++ MultiApp/src/main/scala/droneCop/Utils/HeaderCsv.scala
@@ -0,0 +1,6 @@
+package droneCop.Utils
+
+case class HeaderCsv(header:Array[String]) extends Serializable {
+  val index = header.zipWithIndex.toMap
+  def apply(array:Array[String], key:String):String = array(index(key))
+}
--- MultiApp/src/main/scala/droneCop/Utils/JsonUtils.scala
+++ MultiApp/src/main/scala/droneCop/Utils/JsonUtils.scala
@@ -1,10 +1,9 @@
-
 package droneCop.Utils
 import org.json4s.jackson.JsonMethods._
 import droneCop.Drone.DroneJson
 
-case class jsonUtils() {
-def json2Drone(jsonStr: String): DroneJson = {
+case class JsonUtils() {
+  def json2Drone(jsonStr: String): DroneJson = {
     implicit val formats = org.json4s.DefaultFormats
     parse(jsonStr).camelizeKeys.extract[DroneJson]
   }
