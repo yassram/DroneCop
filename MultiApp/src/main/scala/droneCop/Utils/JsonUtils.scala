f502547d589ff64918d08a380710de49655e16fb
Rayane Amrouche <rayane.amrouche@epita.fr>
2020-05-30 13:58:16 +0200

    [UTILS] update on utils

--- MultiApp/src/main/scala/droneCop/Utils/JsonUtils.scala
+++ MultiApp/src/main/scala/droneCop/Utils/JsonUtils.scala
@@ -1,10 +1,17 @@
 package droneCop.Utils
 import org.json4s.jackson.JsonMethods._
-import droneCop.Drone.DroneJson
+import org.json4s.jackson.Serialization.write
+import droneCop.Drone.DroneViolationJson
 
 case class JsonUtils() {
-  def json2Drone(jsonStr: String): DroneJson = {
+  def json2Drone(jsonStr: String): DroneViolationJson = {
     implicit val formats = org.json4s.DefaultFormats
-    parse(jsonStr).camelizeKeys.extract[DroneJson]
+    parse(jsonStr).camelizeKeys.extract[DroneViolationJson]
+  }
+
+  def drone2Json(jsonStr: DroneViolationJson): String = {
+    implicit val formats = org.json4s.DefaultFormats
+    write(jsonStr)
   }
 }
+
