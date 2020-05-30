c904d83da9a60e51258169536caa9c2116cfc5b5
Yassir RAMDANI <yassir.ramdani@epita.fr>
2020-05-30 19:01:16 +0200

    updated timestamp removed date fixed color, make, body

--- MultiApp/src/main/scala/droneCop/Drone/Drone.scala
+++ MultiApp/src/main/scala/droneCop/Drone/Drone.scala
@@ -1,5 +1,7 @@
 package droneCop.Drone
 import scala.math._
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 case class Location(lat: Double, long: Double)
 case class DroneJson(
@@ -48,8 +50,14 @@ case class DroneMessage(val id: Int) {
 
   val droneId: Int = id
 
-  // time
-  val timestamp: Long = 0
+
+
+  val format = "yyyy/MM/dd HH:mm:ss"
+  val dtf = DateTimeFormatter.ofPattern(format)
+  val ldt = LocalDateTime.now()
+
+  // timestamp
+  val timestamp: String = ldt.format(dtf)
 
   // altitude in meters
   val altitude: Double = 2 + r.nextDouble() * 4
@@ -85,21 +93,44 @@ case class DroneMessage(val id: Int) {
 
 
     //plate
-  val  plateState: String =  List("NY", "NJ", "PA", "CT", "FL", "MA", "IN", "VA", "MD", 
+  val plateState: String =  List("NY", "NJ", "PA", "CT", "FL", "MA", "IN", "VA", "MD", 
     "NC", "99", "IL", "GA", "TX", "AZ", "ME", "OH", "CA", "OK", "SC", "TN", "MI", 
     "DE", "MN", "RI", "NH", "AL", "WA", "VT", "OR", "ON", "QB", "WI", "ID", "KY", 
     "IA", "DC", "MS", "DP", "CO", "MO", "NM", "AR", "LA", "WV", "NV", "SD", "NE", 
     "UT", "KS", "NS", "GV", "AK", "MT", "ND", "HI", "WY", "AB", "PR", "BC", "NB", 
     "PE", "MB", "SK", "FO", "MX", "YT", "NT")(r.nextInt(68))
-  val  plateId: String = plateGen()
-  val  plateType: String = List("0", "1", "2", "3", "4")(r.nextInt(5))
+  val plateId: String = plateGen()
+  val plateType: String = List("0", "1", "2", "3", "4")(r.nextInt(5))
 
   //vehicle
-  val  vehicleColor: String = s"${r.nextInt(30)}"
-  val  vehicleYear: String = s"${1960 + r.nextInt(60)}"
-  val  vehicleMake: String = s"${r.nextInt(30)}"
-  val  vehicleBody: String = s"${r.nextInt(30)}"
-  val  vehicleDate: String = s"${r.nextInt(1220)}"
+  val vehicleColor: String = List("GY", "WH", "WHITE", "BK", "BLACK", "BL", "GREY", "RD", "SILVE", 
+    "BROWN", "BLUE", "RED", "GR", "TN", "GREEN", "null", "OTHER", "YW", "BR", "BLK", "GRAY", "GL", 
+    "TAN", "YELLO", "GOLD", "GRY", "MR", "WHT", "WT", "ORANG", "OR", "WHI", "SL", "LTG", "BRN", 
+    "BLU", "SIL", "LT/", "DK/", "PURPL", "SILVR", "LTGY", "GRN", "DKG", "PR", "GN", "BN", "DKGY", 
+    "DKB", "GYGY", "SILV", "RDW", "WHBL", "MAROO", "BEIGE", "DKBL", "BKGY", "BLW", "BRO", "GLD", 
+    "WHB", "LTB", "WH/", "GY/", "BRWN", "LAVEN", "BLG", "W", "BG", "DKR", "WHO", "BURG", "WHGY", 
+    "GD", "MAR", "YELL", "BRW", "YEL", "DKRD", "NOC", "WHG", "BLGY", "BURGU", "LTBL", "RD/", "BGE", 
+    "GYGR", "GYBL", "DKGR", "SLVR", "BL/", "LTGR", "UNKNO", "SLV", "UNK", "BK/", "YL", "BKBL", "BLWH", 
+    "BWN")(r.nextInt(100))
+  val vehicleYear: String = s"${1960 + r.nextInt(60)}"
+  val vehicleMake: String = List("FORD", "TOYOT", "HONDA", "NISSA", "CHEVR", "FRUEH", 
+    "ME/BE", "DODGE", "BMW", "JEEP", "INTER", "GMC", "HYUND", "LEXUS", "ACURA", "CHRYS", 
+    "VOLKS", "INFIN", "NS/OT", "SUBAR", "ISUZU", "AUDI", "MITSU", "MAZDA", "INCO", "HINO", 
+    "KIA", "CADIL", "MERCU", "VOLVO", "null", "ROVER", "WORKH", "KENWO", "BUICK", "PETER", 
+    "MACK", "PONTI", "MINI", "PORSC", "SATUR", "SMART", "JAGUA", "UD", "WORK", "FIAT", 
+    "SAAB", "UTIL", "MI/F", "SUZUK", "STERL", "VANHO", "OLDSM", "SCION", "VESPA", "MASSA", 
+    "MCI", "PLYMO", "KAWAS", "HUMME", "YAMAH", "HARLE", "UPS", "TRAIL", "IC", "TRIUM", "KW", 
+    "SPRI", "FERRA", "FONTA", "GEO", "BENTL", "THOMA", "PREVO", "NAVIS", "GREAT", "DUCAT", 
+    "WO/C", "VPG", "SMITH", "CHECK", "STARC", "HIN", "UTILI", "TESL", "WHITE", "STAR", "MER", 
+    "EAST", "UT/M", "DORSE", "FHTL", "LND", "FRIE", "FR/LI", "LARO", "PREV", "GIDNY", "TESLA", 
+    "UNIFL")(r.nextInt(100))
+  val vehicleBody: String = List("SUBN","4DSD","VAN","DELV","SDN","2DSD","PICK","REFG","TRAC",
+    "UTIL","TAXI","4DR","BUS","CONV","null","TK","WAGO","4D","TRLR","MCY","P-U","SW","4S",
+    "FLAT","TRAI","T/CR","UT","DUMP","TR/C","TRK","4W","TRUC","VN","STAK","MP","SU","TR","TOW",
+    "2DR","TRL","SEMI","PKUP","SEDN","SD","FOUR","CP","BOAT","SEDA","BS","SWT","MOTO","TANK","2D",
+    "SV","PK","4H","4DO","LIM","4DS","MC","LL","I","TRT","TL","TT","S/SP","TWOD","CON","TRC","TLR",
+    "CV","P/SH","ST","W/DR","STW","SUV","5D","2S","4DSE","HB","MOT","MOBL","H/WH","MCC","PV","TR/E",
+    "COUP","2H","FREI","FODO","HATC","TRAV","LIMO","ES","SN/P","APUR","CMIX","AR","CN","PU")(r.nextInt(100))
 
   def toJsonString(): String = {
     s"""{
@@ -124,7 +155,6 @@ case class DroneMessage(val id: Int) {
     "vehicleYear" :  "${vehicleYear}",
     "vehicleMake" :  "${vehicleMake}",
     "vehicleBody" :  "${vehicleBody}",
-    "vehicleDate" :  "${vehicleDate}"
     """
       } else {
         ""
